39587af016538f3e6a77b1d996f804ef47567d20
Mika Meyer <mimeyer@c2r9s6.42wolfsburg.de>
2025-11-28 20:52:46 +0100

    done

--- get_next_pizza/get_next_line.c
+++ get_next_pizza/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mimeyer <mimeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 20:06:48 by mimeyer           #+#    #+#             */
-/*   Updated: 2025/11/28 20:51:11 by mimeyer          ###   ########.fr       */
+/*   Updated: 2025/11/28 20:52:33 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,6 @@ char *get_next_line(int fd)
 	return(res);
 }
 
-char *malloc_buffer(char **buf, size_t i)
-{
-	buf[i] = malloc(sizeof(char) * BUFFER_SIZE);
-	if (!buf)
-	{
-		free_arr(buf);
-		return (NULL);
-	}
-	return (*buf);
-}
 
 char *read_until(int fd, char **buf)
 {
@@ -80,65 +70,6 @@ void free_arr(char **buf)
 	}
 	free(buf);
 	return ;
-}
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t n)
-{
-	if (dest == 0 || src == 0)
-		return (ft_strlen(src));
-	if (*src == '\0')
-	{
-		*dest = 0;
-		return (ft_strlen(src));
-	}
-	if (n <= 0)
-		return (ft_strlen(src));
-	if (n > ft_strlen(src))
-		n = ft_strlen(src) + 1;
-	dest = ft_memcpy(dest, src, n);
-	dest[n - 1] = '\0';
-	return (ft_strlen(src));
-}
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned int	i;
-	unsigned char	*destt;
-	unsigned char	*srcc;
-
-	destt = (unsigned char *)dest;
-	srcc = (unsigned char *)src;
-	i = 0;
-	while (n > i && (destt != 0 || srcc != 0))
-	{
-		destt[i] = srcc[i];
-		i++;
-	}
-	return (dest);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char *malloc_res(size_t size, char **buf)
-{
-	char *res;
-
-	res = malloc(sizeof(char) * (size + 2));
-	if (!res)
-	{
-		free_arr(buf);
-		free(res);
-		return(NULL);
-	}
-	return(res);
 }
 
 int main(void)
