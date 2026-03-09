b6680291cd84265f1499e671e05baed65a2638b2
Mika Meyer <mimeyer@c1r1s1.42wolfsburg.de>
2026-03-09 16:55:58 +0100

    added main.py to the slop

--- python_slop/mod06/main.py
+++ python_slop/mod06/main.py
@@ -6,4 +6,12 @@ import alchemy
 if __name__ == "__main__":
     print(grimoire.validate_ingredients("fire"))
     print(grimoire.validate_ingredients("shit"))
-    print(grimoire.record_spell("Shitbomb", "fire earth"))
+    print(grimoire.record_spell("Shitbomb", "fire earth") + "\n")
+
+    print(transmutation.lead_to_gold())
+    print(transmutation.philosophers_stone())
+    print(transmutation.stone_to_gem())
+    print(transmutation.elixir_of_life() + "\n")
+
+    print(alchemy.create_water())
+    print(alchemy.create_fire())
